5ac51062a69061664c1dd5f79f48fa1d36a9bc25
RithvikR1218 <rithvik.ravilla@gmail.com>
2023-12-30 13:24:31 +0530

    feat: static file upload

--- welive/receipt_portal/views.py
+++ welive/receipt_portal/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.urls import reverse
+from django.conf import settings
+from django.core.files.storage import FileSystemStorage
+
 # Create your views here.
 
 def say_hello(request):
@@ -11,5 +14,13 @@ def home(request):
     if not request.user.is_authenticated: #if the user is not authenticated
         return HttpResponseRedirect(reverse("login")) #redirect to login page
     else:
-    #code
+        #Logic for uploading files
+        if request.method == 'POST' and request.FILES['myfile']:
+            myfile = request.FILES['myfile']
+            fs = FileSystemStorage()
+            filename = fs.save(myfile.name, myfile)
+            uploaded_file_url = fs.url(filename)
+            return render(request, 'main/home.html', {
+                'uploaded_file_url': uploaded_file_url
+            })
         return render(request,'main/home.html')
