d15fffca640a4cd70e4566512de6a7f18267e781
RithvikR1218 <rithvik.ravilla@gmail.com>
2024-01-10 18:47:12 +0000

    feat: linux to ms migration

--- welive/receipt_portal/views.py
+++ welive/receipt_portal/views.py
@@ -10,9 +10,12 @@ import os
 import glob
 import smtplib
 import shutil
+from docx2pdf import convert
 from email.mime.multipart import MIMEMultipart
 from email.mime.application import MIMEApplication
 from email.mime.text import MIMEText
+import pythoncom
+
 
 # Create your views here.
 def say_hello(request):
@@ -23,12 +26,12 @@ def home(request):
         return HttpResponseRedirect(reverse("login")) #redirect to login page
     else:
         
-        directory = settings.MEDIA_ROOT + "/"
-        files = glob.glob(settings.MEDIA_ROOT+"/*")
+        directory = settings.MEDIA_ROOT + "\\"
+        files = glob.glob(settings.MEDIA_ROOT+"\*")
         file_options = [file.replace(directory, '') for file in files if os.path.isfile(file)]
         
-        receipt_dir = directory + "receipts/"
-        receipts = glob.glob(settings.MEDIA_ROOT+"/receipts/*")
+        receipt_dir = os.path.join(settings.MEDIA_ROOT,"pdf\\")
+        receipts = glob.glob(os.path.join(settings.MEDIA_ROOT,"pdf\*"))
         directory_options = [receipt.replace(receipt_dir, '') for receipt in receipts if os.path.isdir(receipt)]
         
         return render(request,'main/home.html', {
@@ -37,17 +40,17 @@ def home(request):
             })
 
 def generate_receipt(request):
-    #Checking if directory already exists or not
     file = request.POST.get("selected_file")
     stripped_file = os.path.splitext(file)[0]
-    os.mkdir("media/" + "receipts/" + stripped_file)
+    input_directory = os.path.join(settings.MEDIA_ROOT, "word_docs", str(stripped_file))
+    os.makedirs(input_directory, exist_ok=True)
     
     #Add error checking for uploaded file format
-    values = pd.read_excel("media/" + file)
+    file_path = os.path.join(settings.MEDIA_ROOT,file)
+    values = pd.read_excel(file_path)
     slot_no = values["S No"]
     
-    #Create receipt directory if not found
-    doc = DocxTemplate("/home/rithvik/WeLive/welive/media/format/Receipt_Format-4.docx")
+    doc = DocxTemplate(os.path.join(settings.MEDIA_ROOT,"format","Receipt_Format-4.docx"))
     for i in range(len(slot_no)):
         context = { 'donar_name' : values["Name of the Donor"][i],
                     'donor_address': values["Address"][i],
@@ -56,12 +59,15 @@ def generate_receipt(request):
                     'date': values["Donation Date"][i],
                     'receipt_no': slot_no[i]}
         doc.render(context)
-        final_path = "media/receipts/" + stripped_file + "/" + str(slot_no[i]) + "_receipt.docx"
+        final_path=  os.path.join(settings.MEDIA_ROOT,"word_docs",stripped_file,str(slot_no[i]) + "_receipt.docx")
         doc.save(final_path)
-        #Check os and then use one or the other
-        os.system("lowriter --convert-to pdf" +str(" ") + str(final_path))
-        os.system("rm " + str(final_path))
-    os.system("mv *.pdf media/receipts/" + stripped_file)
+
+    #Using microsoft word specific docx2pdf lib
+    output_dir = os.path.join(settings.MEDIA_ROOT, "pdf", str(stripped_file))
+    os.makedirs(output_dir, exist_ok=True)
+    #pythoncom to give python access to word
+    pythoncom.CoInitialize()
+    convert(input_directory,output_dir)
     return HttpResponseRedirect(reverse("home"))
 
 def delete_receipt(request):
@@ -80,12 +86,12 @@ def upload_file(request):
 def send_email(request):
     dir = request.POST.get("selected_directory")
     
-    values = pd.read_excel("media/" + dir + ".xlsx")
+    values = pd.read_excel(os.path.join(settings.MEDIA_ROOT, dir + ".xlsx"))
     slot_no = values["S No"]
     emails = values["Email Address"]
 
-    #Email details
-    email_details = Email.objects.all()
+    #Email details from sqlite
+    email_details = Email.objects.all() 
     body = MIMEText(email_details[0].body) # convert the body to a MIME compatible string
     subject = email_details[0].subject
     sender = email_details[0].sender
@@ -97,8 +103,7 @@ def send_email(request):
         msg['Subject'] = subject
         msg['From'] = sender
         msg['To'] = emails[i]
-        pdf = "media/receipts/" + dir + "/" + str(slot_no[i]) + "_receipt.pdf"
-        
+        pdf = os.path.join(settings.MEDIA_ROOT,"pdf",dir,str(slot_no[i]) + "_receipt.pdf")
         with open(pdf, "rb") as pdf_file:
             pdf_attachment = MIMEApplication(pdf_file.read())
             pdf_attachment.add_header('Content-Disposition', 'attachment', filename= str(slot_no[i]) + "_receipt.pdf")
